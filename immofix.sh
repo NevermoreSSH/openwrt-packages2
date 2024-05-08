d9bb9619c0da1d90adf315d862500471978a08af
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2024-05-08 10:03:37 +0800

    Update immofix.sh

--- immofix.sh
+++ immofix.sh
@@ -2,24 +2,7 @@ sed -i '/irq/d' /etc/crontabs/root;
 sed -i '/irq5/d' /etc/crontabs/root;
 rm -r /etc/config/irq5;
 rm -r /etc/hotplug.d/iface/99-irq4;
-cat <<'EOF' >>/etc/hotplug.d/iface/80-rirq
-
-#!/bin/sh
-
-log () {
-	modlog "$@"
-}
-
-[ "$ACTION" = ifup -o "$ACTION" = ifupdate ] || exit 0
-	sleep 10
-	sh /etc/config/irq4
-  logger "Reloading irqbalance when $INTERFACE ($DEVICE) is now up"
-	fi
-fi
-
-EOF
-sleep 1;
-clear;
+wget -q -O /etc/hotplug.d/iface/80-rirq "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_11dec_owrt/80-rirq";
 echo -e "Success optimize settings . . .";
 read -p "$( echo -e "Press Enter Back to menu . . .")";
 clear;
