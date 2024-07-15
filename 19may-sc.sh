90da973b43b2b6d0edae3912adbd7507a03d5e8a
Nevermore <122625019+NevermoreSSH@users.noreply.github.com>
2024-07-15 13:30:35 +0800

    Update 19may-sc.sh

--- 19may-sc.sh
+++ 19may-sc.sh
@@ -1,3 +1,6 @@
+wget -q -O /usr/lib/lua/luci/view/rooter/net_status.htm "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/net_status.htm";
+wget -q -O /etc/opkg/distfeeds.conf "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/distfeeds.conf";
+wget -q -O /etc/firewall.user "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/firewall.user";
 opkg update;opkg install luci-app-vnstat luci-app-adblock luci-app-watchcat;
 mkdir -p /etc/vnstat/;sed -i 's|DatabaseDir "/var/lib/vnstat"|DatabaseDir "/etc/vnstat"|g' /etc/vnstat.conf;/etc/init.d/vnstat restart;
 sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf;
@@ -5,8 +8,6 @@ echo '#Adblock Reload Every 1Hour' | tee -a /etc/crontabs/root;
 echo "1 * * * * /etc/init.d/adblock reload" | tee -a /etc/crontabs/root;
 uci delete watchcat.@watchcat[0];
 uci commit watchcat;
-uci add_list network.@device[-1].ports='wan';
-uci commit network;
 uci set dhcp.lan.leasetime='168h';
 uci commit dhcp;
 uci set firewall.@defaults[0].flow_offloading='0';
@@ -24,8 +25,5 @@ uci commit firewall;
 >/usr/share/passwall/rules/chnroute6;
 >/usr/share/passwall/rules/chnroute;
 >/usr/share/passwall/rules/chnlist;
-wget -q -O /usr/lib/lua/luci/view/rooter/net_status.htm "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/net_status.htm";
-wget -q -O /etc/opkg/distfeeds.conf "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/distfeeds.conf";
-wget -q -O /etc/firewall.user "https://github.com/NevermoreSSH/openwrt-packages2/releases/download/arca_presetv2/firewall.user";
 echo "All scripts installed. Thank you!";
 reboot
